5ed329e6539f9e6da71df0c581eec04389ce9d6c
Tom Söderlund <tom@tomsoderlund.com>
2016-05-20 22:59:16 +0200

    Instagram fix with request.get

--- config/routes.js
+++ config/routes.js
@@ -6,15 +6,19 @@
 
 var express = require('express');
 var url = require('url');
+var request = require('request');
 var proxy = require('express-http-proxy');
 
 
 module.exports = function (app, config) {
 
+	// ----- YAHOO -----
+
 	// https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys
 	// curl -v "localhost:3014/yahoo/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
 	var yahooPrefix = '/yahoo';
-	var yahooProxy = proxy('query.yahooapis.com', {
+	var yahooHost = 'query.yahooapis.com';
+	var yahooProxy = proxy(yahooHost, {
 		forwardPath: function (req, res) {
 			var trimmedPath = req.originalUrl.substring(yahooPrefix.length);
 			var newPath = url.parse(trimmedPath).path;
@@ -24,17 +28,32 @@ module.exports = function (app, config) {
 	});
 	app.use(yahooPrefix + '/*', yahooProxy);
 
+
+	// ----- INSTAGRAM -----
+
 	// https://api.instagram.com/v1/tags/design/media/recent?access_token=XXX
 	// curl -v localhost:3014/instagram/publicapi/v1/tags/design/media/recent?access_token=XXX
 	var instagramPrefix = '/instagram';
-	var instagramProxy = proxy('api.instagram.com', {
-		forwardPath: function (req, res) {
-			var trimmedPath = req.originalUrl.substring(instagramPrefix.length);
-			var newPath = url.parse(trimmedPath).path;
-			//console.log('URLs', trimmedPath, newPath, {originalUrl:req.originalUrl, url:req.url, baseUrl:req.baseUrl});
-			return newPath;
-		}
-	});
-	app.use(instagramPrefix + '/*', instagramProxy);
+	var instagramHost = 'api.instagram.com';
+
+	// var instagramProxy = proxy(instagramHost, {
+	// 	forwardPath: function (req, res) {
+	// 		var trimmedPath = req.originalUrl.substring(instagramPrefix.length);
+	// 		var newPath = url.parse(trimmedPath).path;
+	// 		//console.log('URLs', trimmedPath, newPath, {originalUrl:req.originalUrl, url:req.url, baseUrl:req.baseUrl});
+	// 		return newPath;
+	// 	}
+	// });
+
+	var requestInstagram = function (req, res) {
+		var trimmedPath = req.originalUrl.substring(instagramPrefix.length);
+		var newPath = url.parse(trimmedPath).path;
+		var completePath = 'https://' + instagramHost + newPath;
+		request.get(completePath, { json: true }, function (err, response, data) {
+			return res.status(response.statusCode).json(data);
+		});
+	}
+
+	app.use(instagramPrefix + '/*', requestInstagram);
 
 };
